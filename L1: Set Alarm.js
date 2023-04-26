4b0fdab39f3e4077bafb48e381a7f2d9cdcddae3
BSN89 <barakovsn55@gmail.com>
2023-04-26 23:04:11 +0300

    26 april

---
+++ L1: Set Alarm.js
@@ -0,0 +1,11 @@
+function setAlarm(employed, vacation){
+    return employed===true && vacation===false ? true : false
+    
+
+    // или такой вариант
+  //if(employed===true && vacation===false){
+   // return true
+  //}else{
+   // return false
+  //}
+  }
