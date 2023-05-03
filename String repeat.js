2b15ce4bc39ab5cbb0bdea36872b703ad5628e70
BSN89 <barakovsn55@gmail.com>
2023-05-03 12:57:51 +0300

    3 may

---
+++ String repeat.js
@@ -0,0 +1,10 @@
+//Write a function that accepts an integer n and a string s as parameters, 
+//and returns a string of s repeated exactly n times.
+
+
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+  const result = repeatStr (5, 'Hi')
+  console.log(result)
