8608ac2c3f3c4c10eb9bc07f387df31076da59f1
BSN89 <barakovsn55@gmail.com>
2023-06-30 09:46:18 +0300

    30 June

---
+++ 8 kyu Drink about.js
@@ -0,0 +1,16 @@
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+const peopleWithAgeDrink = (old) =>
+  "drink " +
+  (old < 14 ? "toddy" : old < 18 ? "coke" : old < 21 ? "beer" : "whisky");
