a6849ea013530240533342fb3ef6a127af829a3c
sergej <barakovsn55@gmail.com>
2024-05-10 21:11:53 +0300

    10 may

---
+++ 6 kyu Take a Ten Minutes Walk.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+let arr = ['n','s','n','s','n','s','n','s','n','s', 'r']
+
+function isValidWalk(walk) {
+    if(walk.length === 10){
+      return true
+    } 
+    return false
+  }
+  console.log(isValidWalk(arr));
