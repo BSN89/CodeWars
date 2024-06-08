098ca83488460fa939c9e267fa4a495593244b46
BSN89 <barakovsn55@gmail.com>
2024-06-08 23:35:50 +0300

    8 june

---
+++ 8 kyu Reversed Words.js
@@ -0,0 +1,28 @@
+// Complete the solution so that it reverses all 
+// of the words within the string passed in.
+
+// Words are separated by exactly one space and 
+// there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+ 
+// let firstString = "The greatest victory is that which requires no battle"
+
+// function reverseWords(str){
+//     return str.split(' ').reverse().join(' ')
+//   }
+
+//   console.log(reverseWords(firstString));
+
+var countSheep = function (num){
+    
+    for(let i = 0; i <= num; i++){
+        i+1
+        return  `${i} sheep...`
+      }
+  }
+    
+    console.log(countSheep(4));
