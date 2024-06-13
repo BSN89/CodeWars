9d0a822c907d13e5911e62b4b1546dfe50fa8340
BSN89 <barakovsn55@gmail.com>
2024-06-13 23:33:02 +0300

    13 june

--- 6 kyu Unique In Order.js
+++ 6 kyu Unique In Order.js
@@ -9,14 +9,13 @@
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 
 var uniqueInOrder=function(iterable){
-    let result = []
-    let newArr  = iterable.split('')
-    newArr.forEach(el => {
-        if(!result.includes(el)){
-            result.push(el)
-        }
-    });
-    return result
+    let arr = []
+     for(let i = 0; i <= iterable.length; i++){
+         if(iterable[i] !== iterable[i - 1]){
+             arr.push(iterable[i])
+         }
+     }
+    return arr
    }
 
    console.log(uniqueInOrder('AAAABBBCCDAABBB'));
