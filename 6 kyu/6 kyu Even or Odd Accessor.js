d786e7370d13aa991fb1157868d319599493098a
BSN89 <barakovsn55@gmail.com>
2024-07-02 18:56:32 +0300

    2 jule

---
+++ 6 kyu/6 kyu Even or Odd Accessor.js
@@ -0,0 +1,27 @@
+// Create a function or callable object that takes an integer as 
+// an argument and returns "Even" for even numbers or "Odd" for odd numbers. 
+// The function should also return "Even" or "Odd" when accessing a value at an integer index.
+
+// For example:
+
+// evenOrOdd(2); //'Even'
+// evenOrOdd[2]; //'Even'
+// evenOrOdd(7); //'Odd'
+// evenOrOdd[7]; //'Odd'
+
+
+function evenOrOdd(n) {
+    if(Array.isArray(n)){
+      let num = n.pop()
+      console.log( num)
+
+      return num % 2 === 0 ? 'Even': 'Odd'
+    }
+    return n % 2 === 0 ? 'Even': 'Odd'
+  }
+
+  console.log(evenOrOdd([4]));
+   console.log(evenOrOdd(2));
+  console.log(evenOrOdd([11]));
+  console.log(evenOrOdd(7));
+  console.log(evenOrOdd([-44]));
