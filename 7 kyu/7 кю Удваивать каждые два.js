e7338b0c43d37882b6f12b5d98bbaf11122ba7eb
BSN89 <barakovsn55@gmail.com>
2024-09-25 14:03:14 +0300

    25 september

--- 7 kyu/7 кю Удваивать каждые два.js
+++ 7 kyu/7 кю Удваивать каждые два.js
@@ -11,17 +11,19 @@
 // [1,4,3,8]
 
 
-function doubleEveryOther(a) {
-    let res = []
-    for (let i = 0; i < a.length; i++) {
-        if(i % 2 !== 0){
-            res.push(a[i] * 2) 
-        }else {
-            res.push(a[i])
-        }
-    }
-    return res
-  }
+// function doubleEveryOther(a) {
+//     let res = []
+//     for (let i = 0; i < a.length; i++) {
+//         if(i % 2 !== 0){
+//             res.push(a[i] * 2) 
+//         }else {
+//             res.push(a[i])
+//         }
+//     }
+//     return res
+//   }
+
+const doubleEveryOther = a => a.map((el,i) => i % 2 !== 0 ? el * 2 : el ) 
 
   console.log(doubleEveryOther([1,2,3,4]));
   console.log(doubleEveryOther([1,19,6,2,12,-3]));
