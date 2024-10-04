29b8dbb900b5cab78059eb87e1efc9f726d15477
BSN89 <barakovsn55@gmail.com>
2024-10-04 23:26:54 +0300

    4 oct

--- 7 kyu/7 кю Мои языковые навыки.js
+++ 7 kyu/7 кю Мои языковые навыки.js
@@ -11,5 +11,20 @@
 // {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
 
 function myLanguages(results) {
+    
+    let arrIsObj = Object.entries(results).sort((a,b) => b[1] - a[1])
+    let objIsArr = Object.fromEntries(arrIsObj)
+    
+    let result = []
+       for (const key in objIsArr) {
+          if (objIsArr[key] >= 60) {
+            result.push(key)
+          }
+        }
+     return result
+   }
 
-}
+
+console.log(myLanguages({"Java" : 10, "Ruby" : 80, "Python" : 65}));
+console.log(myLanguages({"Hindi" : 60, "Greek" : 71, "Dutch" : 93}));
+console.log(myLanguages({"C++" : 50, "ASM" : 10, "Haskell" : 20}));
