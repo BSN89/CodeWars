20da279aa7beb27e265790f994aac1fed24c3091
BSN89 <barakovsn55@gmail.com>
2024-12-23 21:57:00 +0300

    23 dec

--- 7 kyu/7 кю Не ставь мне пять!.js
+++ 7 kyu/7 кю Не ставь мне пять!.js
@@ -16,7 +16,8 @@ function dontGiveMeFive(start, end){
     let arr = []
     for (let i = start; i <= end; i++) {
         if(!i.toString().includes("5")){
-            arr.push(i)
+            arr.push(i) 
+            
         } 
     }
   return arr.length
